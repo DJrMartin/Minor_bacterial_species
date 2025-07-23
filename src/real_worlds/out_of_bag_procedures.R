30580174e59a572b6b1dfe1b9c6214b6c81239b6
David_MacOS <david.martin@univ-rennes2.fr>
2025-07-23 09:46:18 +0200

    update

--- src/real_worlds/out_of_bag_procedures.R
+++ src/real_worlds/out_of_bag_procedures.R
@@ -4,6 +4,7 @@ rm(list=ls())
 load("data/data.rda")
 source("functions/out_of_bag.R")
 
+#==================================================================================
 ### MARTIN ET AL. (2025) ###
 ## X
 X <- as.data.frame(t(apply(data$`Martin et al.`$count, 2, function(x) x/sum(x))))
@@ -14,13 +15,15 @@ Y <- as.factor(FO>0.4)
 
 res.martin = out_of_bag_prediction(X, Y, cv = 50)
 # boxplot(res.martin$X, res.martin$R) ; t.test(res.martin$X, res.martin$R)
+#==================================================================================
 
+#==================================================================================
 ### WU ET AL. (2024) ###
 X <- as.data.frame(t(apply(data$`Wu et al.`[,-c(1:4)], 1, function(x) x/sum(x))))
 # rowSums(X)
 ## Y
 Y <- as.factor(data$`Wu et al.`$group)
 
-res.wu = out_of_bag_prediction(X, Y, cv = 50)
+res.wu = out_of_bag_prediction(X, Y, cv = 20)
 # boxplot(res.wu$X, res.wu$R) ; t.test(res.wu$X, res.wu$R)
-
+#==================================================================================
